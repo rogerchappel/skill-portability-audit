35ad9d78e2b22b874a82f24cdc8177571f636881
Roger Chappel <miscanalysis@gmail.com>
2026-08-17 19:29:28 +1000

    fix: scope approvals at semicolon boundaries

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ function hasAffirmativeApprovalLanguage(text) {
 }
 
 function findUnapprovedSideEffects(text) {
-  const statements = text.split(/(?<=[.!?])(?:[ \t]+|\r?\n+)|\r?\n+/);
+  const statements = text.split(/(?<=[.!?])(?:[ \t]+|\r?\n+)|\r?\n+|\s*;\s*/);
   const contrastiveBoundary = /\s*(?:[,;:]\s*)?\b(?:but|however|whereas|while)\b\s*/i;
   const sideEffect = /\b(publish(?:es|ed|ing)?|post(?:s|ed|ing)?|deploy(?:s|ed|ing)?|send(?:s|ing)?|sent|messag(?:e|es|ed|ing)|delet(?:e|es|ed|ing)|merg(?:e|es|ed|ing)|charg(?:e|es|ed|ing)|email(?:s|ed|ing)?)\b/gi;
   const findings = [];
